0ff48ddaefe6e7738c9139018f87fef5f918e2f1
QuocTuan <tranquoctuan31012001@gmail.com>
2023-04-27 21:37:32 +0700

    load file

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from transformers import pipeline
+import docx2txt
 
 app = Flask(__name__)
 
@@ -7,26 +8,28 @@ app = Flask(__name__)
 @app.route('/answer', methods=['POST'])
 def answer():
     data = request.form
-    f = request.files
+    # f = request.files
     # load request with 2 parameters: questions and contexts
     question = data['questions']
     # context = 'Hello'
     # context = data['contexts']
-    context = f['file']
+    context = docx2txt.process("HCM.docx")
+    print(context)
     # context = f.filename
     question_answerer = pipeline(
-        "question-answering", model="../model/train_bert_transformer/checkpoint-3000")
+        "question-answering", model="C:/Users/tranq/Documents/Semeter1_2022_2023/kltn/model/checkpoint-3000")
     response = question_answerer(question=question, context=context)
-    # return response['answer']
-    return render_template("./index.html", question=question, context=context, answer=response['answer'], name = context.filename)
+    return response['answer']
+    # return render_template("./index.html", question=question, context=context, answer=response['answer'], name=context.filename)
 
-@app.route('/success', methods = ['POST'])  
-def success():  
-    if request.method == 'POST':  
+
+@app.route('/success', methods=['POST'])
+def success():
+    if request.method == 'POST':
         f = request.files['file']
-        f.save(f.filename)  
-        return render_template("index.html", name = f.filename)
-    
+        f.save(f.filename)
+        return render_template("index.html", name=f.filename)
+
 
 @app.route('/')
 def upload():
@@ -35,7 +38,6 @@ def upload():
 # @app.route('/')
 # def index():
 #     return render_template('index.html')
-
 
 
 if __name__ == '__main__':
