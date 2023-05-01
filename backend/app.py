995223c2ca24eb0eb42a5e660aa7f93b7d9e2052
QuocTuan <tranquoctuan31012001@gmail.com>
2023-05-01 08:37:55 +0700

    merge develop

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,41 @@
 from flask import Flask, render_template, request
 from transformers import pipeline
+import docx2txt
+
 app = Flask(__name__)
+
+
+@app.route('/success', methods=['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        return render_template("index.html", name=f.filename)
 
 
 @app.route('/answer', methods=['POST'])
 def answer():
     data = request.form
+    # f = request.files
     # load request with 2 parameters: questions and contexts
     question = data['questions']
-    context = data['contexts']
+    context = docx2txt.process("HCM.docx")
+    # context = docx2txt.process(f)
+    # print(context)
     question_answerer = pipeline(
-        "question-answering", model="../model/train_bert_transformer/checkpoint-3000")  # change url when run
+        "question-answering", model="../model/checkpoint-3000")
     response = question_answerer(question=question, context=context)
     # return response['answer']
     return render_template("./index.html", question=question, context=context, answer=response['answer'])
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def upload():
+    return render_template('upload.html')
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 
 if __name__ == '__main__':
