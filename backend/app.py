8bca21c2d6b1b2da7a7ba10f120fa262d5042f7b
lamson11 <lamson8204@gmail.com>
2023-05-13 17:14:58 +0700

    fix_upload2.html

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ DEVICE = torch.device('cpu')
 phobert = AutoModel.from_pretrained("vinai/phobert-large")
 tokenizer_2 = AutoTokenizer.from_pretrained("vinai/phobert-large")
 model_2 = RobertaForQuestionAnswering(phobert.config).from_pretrained(
-    "../model/phobert_model").to(DEVICE)
+    "../model/pho-bert/final_model").to(DEVICE)
 
 app = Flask(__name__)
 
@@ -64,7 +64,7 @@ def success():
 
 @app.route('/')
 def upload():
-    return render_template('upload.html')
+    return render_template('upload2.html')
 
 
 @app.route('/answer', methods=['POST'])
